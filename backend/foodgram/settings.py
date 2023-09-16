4bc22a053e465f0bc7c84cff2e93a3c6ee3ce972
Александр Рузанов <blacksam2013@gmail.com>
2023-09-16 22:06:59 +0700

    testfix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'NAME': os.getenv('POSTGRES_DB', 'django_db'),
+        'USER': os.getenv('POSTGRES_USER', 'django_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
