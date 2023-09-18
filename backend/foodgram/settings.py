fd1f98139d323020b1611208587e88bf5e299f2a
Александр Рузанов <blacksam2013@gmail.com>
2023-09-18 23:50:41 +0700

    fix2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-1y8z%c#ynaxrk8r_qqxmvtgi7n!pixq$(548_j0c1u*o16lnl1')
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
