7ddc5c7d907c8f1b28431d27393daa0c5aebc1c8
Александр Рузанов <blacksam2013@gmail.com>
2023-09-22 20:30:56 +0700

    nginx_fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret
+# SECURITY WARNING: keep the secret key used in production secret -3-
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-1y8z%c#ynaxrk8r_qqxmvtgi7n!pixq$(548_j0c1u*o16lnl1')
 # SECURITY WARNING: don't run with debug turned on in production
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
