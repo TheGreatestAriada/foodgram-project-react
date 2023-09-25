7fd73bd070ae72979e9a7384fec9eb195d3b7655
Александр Рузанов <blacksam2013@gmail.com>
2023-09-25 16:04:17 +0700

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-1y8z%c#ynaxrk8r_qqxmvtgi7n
 # SECURITY WARNING: don't run with debug turned on in production
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
-
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', "*").split(',')
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -79,12 +79,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django_db'),
         'USER': os.getenv('POSTGRES_USER', 'django_user'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'django_password'),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
