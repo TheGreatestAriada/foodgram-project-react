f8f44fee5992eafbf1e5a6d3f623ba1ea589e6ac
Александр Рузанов <blacksam2013@gmail.com>
2023-09-25 16:27:37 +0700

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,10 +1,6 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'///
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret -3-
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-1y8z%c#ynaxrk8r_qqxmvtgi7n!pixq$(548_j0c1u*o16lnl1')
+SECRET_KEY = os.getenv('SECRET_KEY', ' ')
 # SECURITY WARNING: don't run with debug turned on in production
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', "*").split(',')
 ALLOWED_HOSTS = ["*"]
